e78d6d9b5ed3a5e470141f0f290aff59aa81a3c5
Atul Yadav <atulyadav5566@live.com>
2021-06-14 22:16:05 +0530

    Auto-generated commit [ci skip]

---
+++ assets/appConstants/routeConstant.ts
@@ -0,0 +1,26 @@
+export const routeConfig =[
+
+    { 
+        routeLink: '',
+        routeLinkText:'Home',
+        routeComponent:''
+    },
+
+    { 
+        routeLink: '',
+        routeLinkText:'Admission',
+        routeComponent:''
+    },
+
+    { 
+        routeLink: '',
+        routeLinkText:'Results',
+        routeComponent:''
+    },
+
+    { 
+        routeLink: '',
+        routeLinkText:'Management',
+        routeComponent:''
+    }
+]
